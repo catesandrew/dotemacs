b98eb11aa96f21da9797149e247dc6d7ad9ac6e4
Andrew Cates <catesandrew@gmail.com>
2015-05-04 11:49:30 -0700

    Disable relative line numbers since they are slow

--- config/init-eyecandy.el
+++ config/init-eyecandy.el
@@ -104,8 +104,6 @@
 (add-hook 'find-file-hook 'hl-line-mode)
 
 
-;; relative line numbers
-(add-hook 'prog-mode-hook 'relative-line-numbers-mode t)
 (add-hook 'prog-mode-hook 'line-number-mode t)
 (add-hook 'prog-mode-hook 'column-number-mode t)
 
