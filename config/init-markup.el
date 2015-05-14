faa0ad72e553bd6a55cba23b732147a924af7975
Andrew Cates <catesandrew@gmail.com>
2015-05-13 21:10:45 -0700

    Map apib to markdown mode, open in Marked 2.app

--- config/init-markup.el
+++ config/init-markup.el
@@ -1,5 +1,6 @@
 (lazy-major-mode "\\.md$" markdown-mode)
 (lazy-major-mode "\\.markdown$" markdown-mode)
+(lazy-major-mode "\\.apib$" markdown-mode)
 (lazy-major-mode "\\.toml$" toml-mode)
 (lazy-major-mode "\\.yaml$" yaml-mode)
 (lazy-major-mode "\\.yml$" yaml-mode)
@@ -8,6 +9,8 @@
 (add-hook 'yaml-mode-hook (lambda () (electric-indent-local-mode -1)))
 (add-hook 'toml-mode-hook (lambda () (electric-indent-local-mode -1)))
 (add-hook 'markdown-mode-hook (lambda () (electric-indent-local-mode -1)))
+
+;; TODO: gfm-mode
 
 ;; markdown
 (setq markdown-imenu-generic-expression
@@ -29,5 +32,15 @@
 (when on_darwin
   (setq markdown-open-command "mark"))
 
-(provide 'init-markup)
+(defun markdown-preview-file ()
+  "use Marked 2 to preview the current file"
+  (interactive)
+  (shell-command
+   (format "open -a 'Marked 2.app' %s"
+       (shell-quote-argument (buffer-file-name)))))
 
+; (when on_darwin
+;   (global-set-key (kbd "C-cm") 'markdown-preview-file))
+
+
+(provide 'init-markup)
