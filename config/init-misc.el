db6eb9de420d06255cdb6e10af65293aeb91318e
Andrew Cates <catesandrew@gmail.com>
2015-05-13 21:04:50 -0700

    Persistent undo in emacs

--- config/init-misc.el
+++ config/init-misc.el
@@ -134,8 +134,11 @@
 (setq undo-tree-auto-save-history t)
 (setq undo-tree-history-directory-alist
       `(("." . ,(concat dotemacs-cache-directory "undo"))))
+    (unless (file-exists-p (concat dotemacs-cache-directory "undo"))
+        (make-directory (concat dotemacs-cache-directory "undo")))
 (setq undo-tree-visualizer-timestamps t)
 (setq undo-tree-visualizer-diff t)
+
 
 
 (after 'evil
