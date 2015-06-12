c5885885dac8f70ecdd115650dec182266546c83
Andrew Cates <catesandrew@gmail.com>
2015-06-11 22:42:08 -0700

    emacs 25 wants (package-initialize) in init.el

--- init.el
+++ init.el
@@ -110,10 +110,23 @@
 ;; Please don't load outdated byte code
 (setq load-prefer-newer t)
 
-(require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '(("melpa" . "http://melpa.org/packages/")))
 (add-to-list 'package-archives '(("org" . "http://orgmode.org/elpa/")))
+
+;; The reason automatic package loading occurs after loading the init file is
+;; that user options only receive their customized values after loading the init
+;; file, including user options which affect the packaging system. In some
+;; circumstances, you may want to load packages explicitly in your init file
+;; (usually because some other code in your init file depends on a package). In
+;; that case, your init file should call the function `package-initialize`. It is
+;; up to you to ensure that relevant user options, such as `package-load-list`
+;; (see below), are set up prior to the `package-initialize` call. You should also
+;; set `package-enable-at-startup` to `nil`, to avoid loading the packages again
+;; after processing the init file. Alternatively, you may choose to completely
+;; inhibit package loading at startup, and invoke the command
+;; `M-x package-initialize` to load your packages manually.
+;; http://stackoverflow.com/questions/11127109/
+(setq package-enable-at-startup nil)
 (package-initialize)
 
 ;; Bootstrap `use-package'
