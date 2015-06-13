869b34a8f437bfc5dfa1a2dd44674c23cbf9c5a7
Andrew Cates <catesandrew@gmail.com>
2015-06-12 19:15:47 -0700

    Remove emacs custom from init.el

--- init.el
+++ init.el
@@ -729,15 +729,3 @@ mouse-3: go to end"))))
 ;           (lambda ()
 ;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
 ;               (ggtags-mode 1))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exec-path-from-shell use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
