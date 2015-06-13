13a043853dd6c77fd9a5e34e61de81d28120d918
Andrew Cates <catesandrew@gmail.com>
2015-06-12 19:09:25 -0700

    Use desktop to load/save emacs sessions

--- config/init-misc.el
+++ config/init-misc.el
@@ -262,45 +262,6 @@
      )))
 
 
-; Sessions (:mksession in Vim)
-
-; Emacs have the commands M-x desktop-save and desktop-read. To have it
-; automatically saved/restored put into the .emacs: (desktop-save-mode 1). If
-; you want to start emacs without auto loading the session (if you configured
-; it), the command is emacs --no-desktop. But Emacs sessions doesn't know about
-; elscreens (which evil-tabs use for creating Vim-like tabs) so if you want to
-; save and restore full sessions use these functions:
-
-;; Save session including tabs
-;; http://stackoverflow.com/questions/22445670/save-and-restore-elscreen-tabs-and-split-frames
-(defun session-save ()
-    "Store the elscreen tab configuration."
-    (interactive)
-    (if (desktop-save user-emacs-directory)
-        (with-temp-file (concat user-emacs-directory ".elscreen")
-            (insert (prin1-to-string (elscreen-get-screen-to-name-alist))))))
-
-;; Load session including tabs
-(defun session-load ()
-    "Restore the elscreen tab configuration."
-    (interactive)
-    (if (desktop-read)
-        (let ((screens (reverse
-                        (read
-                         (with-temp-buffer
-                          (insert-file-contents (concat user-emacs-directory ".elscreen"))
-                          (buffer-string))))))
-            (while screens
-                (setq screen (car (car screens)))
-                (setq buffers (split-string (cdr (car screens)) ":"))
-                (if (eq screen 0)
-                    (switch-to-buffer (car buffers))
-                    (elscreen-find-and-goto-by-buffer (car buffers) t t))
-                (while (cdr buffers)
-                    (switch-to-buffer-other-window (car (cdr buffers)))
-                    (setq buffers (cdr buffers)))
-                (setq screens (cdr screens))))))
-
 ; Here, I define a "helm-my-buffers" function that when called will show Helm
 ; interface but searching (fuzzy, real time as you write, unordered) in open
 ; buffers, recent files, project files (see below for more on that), tags
