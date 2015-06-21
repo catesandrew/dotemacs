8c4d2a1d2488a160642a2f9e035ab2458e00a61c
Andrew Cates <catesandrew@gmail.com>
2015-06-20 21:37:01 -0700

    Remove comment code

--- init.el
+++ init.el
@@ -3437,29 +3437,6 @@ Disable the highlighting of overlong lines."
 
 ;;; Version control
 
-;; TODO: Incorporate this code from `init-vcs`
-;; original `init-vcs`
-; (with-eval-after-load 'vc-git
-;   (with-eval-after-load 'evil
-;     (with-eval-after-load 'magit-blame
-;       (defadvice magit-blame-file-on (after advice-for-magit-blame-file-on activate)
-;         (evil-emacs-state))
-;       (defadvice magit-blame-file-off (after advice-for-magit-blame-file-off activate)
-;         (evil-exit-emacs-state))))
-;
-;   (if (display-graphic-p)
-;       (progn
-;         (require 'git-gutter-fringe+))
-;     (require 'git-gutter+))
-;
-;   (global-git-gutter+-mode))
-;
-; (require 'diff-hl)
-; (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-; (unless (display-graphic-p)
-;   (diff-hl-margin-mode))
-;; end origianl `init-vcs`
-
 (use-package vc-hooks                   ; Simple version control
   :defer t
   :config
