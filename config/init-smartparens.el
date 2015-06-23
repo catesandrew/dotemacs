e51cbdcda2d06a22024a9a0ade10baa1e328a84e
Andrew Cates <catesandrew@gmail.com>
2015-06-23 12:37:12 -0700

    Fix for delay loading of smartparens

--- config/init-smartparens.el
+++ config/init-smartparens.el
@@ -8,51 +8,6 @@
 ;; additional pairs.
 
 ;;; Code:
-(require 'smartparens-config)
-
-
-;;; Additional pairs for various modes
-
-;; Emacs Lisp
-(sp-local-pair '(emacs-lisp-mode lisp-interaction-mode inferior-emacs-lisp-mode)
-               "(" nil :bind "M-(")
-
-
-;;; Key bindings
-
-(let ((map smartparens-mode-map))
-  ;; Movement and navigation
-  (define-key map (kbd "C-M-f") #'sp-forward-sexp)
-  (define-key map (kbd "C-M-b") #'sp-backward-sexp)
-  (define-key map (kbd "C-M-u") #'sp-backward-up-sexp)
-  (define-key map (kbd "C-M-d") #'sp-down-sexp)
-  (define-key map (kbd "C-M-p") #'sp-backward-down-sexp)
-  (define-key map (kbd "C-M-n") #'sp-up-sexp)
-  ;; Deleting and killing
-  (define-key map (kbd "C-M-k") #'sp-kill-sexp)
-  (define-key map (kbd "C-M-w") #'sp-copy-sexp)
-  ;; Depth changing
-  (define-key map (kbd "M-S-<up>") #'sp-splice-sexp)
-  (define-key map (kbd "M-<up>") #'sp-splice-sexp-killing-backward)
-  (define-key map (kbd "M-<down>") #'sp-splice-sexp-killing-forward)
-  (define-key map (kbd "M-C-<up>") #'sp-splice-sexp-killing-around)
-  (define-key map (kbd "M-?") #'sp-convolute-sexp)
-  ;; Barfage & Slurpage
-  (define-key map (kbd "C-)") #'sp-forward-slurp-sexp)
-  (define-key map (kbd "C-<right>") #'sp-forward-slurp-sexp)
-  (define-key map (kbd "C-}") #'sp-forward-barf-sexp)
-  (define-key map (kbd "C-<left>") #'sp-forward-barf-sexp)
-  (define-key map (kbd "C-(") #'sp-backward-slurp-sexp)
-  (define-key map (kbd "C-M-<left>") #'sp-backward-slurp-sexp)
-  (define-key map (kbd "C-{") #'sp-backward-barf-sexp)
-  (define-key map (kbd "C-M-<right>") #'sp-backward-barf-sexp)
-  ;; Miscellaneous commands
-  (define-key map (kbd "M-S") #'sp-split-sexp)
-  (define-key map (kbd "M-J") #'sp-join-sexp)
-  (define-key map (kbd "C-M-t") #'sp-transpose-sexp))
-
-(let ((map smartparens-strict-mode-map))
-  (define-key map (kbd "M-q") #'sp-indent-defun))
 
 ;; enable smartparens-mode in `eval-expression'
 (defun dotemacs-conditionally-enable-smartparens-mode ()
--- init.el
+++ init.el
@@ -1953,6 +1953,48 @@ mouse-3: go to end"))))
   (progn
     (require 'smartparens-config)
 
+    ;;; Additional pairs for various modes
+
+    ;; Emacs Lisp
+    (sp-local-pair '(emacs-lisp-mode lisp-interaction-mode inferior-emacs-lisp-mode)
+                   "(" nil :bind "M-(")
+
+    ;;; Key bindings
+
+    (let ((map smartparens-mode-map))
+      ;; Movement and navigation
+      (define-key map (kbd "C-M-f") #'sp-forward-sexp)
+      (define-key map (kbd "C-M-b") #'sp-backward-sexp)
+      (define-key map (kbd "C-M-u") #'sp-backward-up-sexp)
+      (define-key map (kbd "C-M-d") #'sp-down-sexp)
+      (define-key map (kbd "C-M-p") #'sp-backward-down-sexp)
+      (define-key map (kbd "C-M-n") #'sp-up-sexp)
+      ;; Deleting and killing
+      (define-key map (kbd "C-M-k") #'sp-kill-sexp)
+      (define-key map (kbd "C-M-w") #'sp-copy-sexp)
+      ;; Depth changing
+      (define-key map (kbd "M-S-<up>") #'sp-splice-sexp)
+      (define-key map (kbd "M-<up>") #'sp-splice-sexp-killing-backward)
+      (define-key map (kbd "M-<down>") #'sp-splice-sexp-killing-forward)
+      (define-key map (kbd "M-C-<up>") #'sp-splice-sexp-killing-around)
+      (define-key map (kbd "M-?") #'sp-convolute-sexp)
+      ;; Barfage & Slurpage
+      (define-key map (kbd "C-)") #'sp-forward-slurp-sexp)
+      (define-key map (kbd "C-<right>") #'sp-forward-slurp-sexp)
+      (define-key map (kbd "C-}") #'sp-forward-barf-sexp)
+      (define-key map (kbd "C-<left>") #'sp-forward-barf-sexp)
+      (define-key map (kbd "C-(") #'sp-backward-slurp-sexp)
+      (define-key map (kbd "C-M-<left>") #'sp-backward-slurp-sexp)
+      (define-key map (kbd "C-{") #'sp-backward-barf-sexp)
+      (define-key map (kbd "C-M-<right>") #'sp-backward-barf-sexp)
+      ;; Miscellaneous commands
+      (define-key map (kbd "M-S") #'sp-split-sexp)
+      (define-key map (kbd "M-J") #'sp-join-sexp)
+      (define-key map (kbd "C-M-t") #'sp-transpose-sexp))
+
+    (let ((map smartparens-strict-mode-map))
+      (define-key map (kbd "M-q") #'sp-indent-defun))
+
     (setq sp-autoskip-closing-pair 'always
           ;; Don't kill entire symbol on C-k
           sp-hybrid-kill-entire-symbol nil)
