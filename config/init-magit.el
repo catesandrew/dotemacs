4783d5c56a2fb6eeafdba7c49e8fd31dfcec7cde
Andrew Cates <catesandrew@gmail.com>
2015-07-07 11:30:48 -0700

    Remove unused functions

--- config/init-magit.el
+++ config/init-magit.el
@@ -1,23 +1,3 @@
-;; ignore whitespace
-;;;###autoload
-(defun dotemacs-magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
-
-;;;###autoload
-(defun dotemacs-magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
-
-;;;###autoload
-(defun dotemacs-magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
-
 (defun dotemacs-load-gh-pulls-mode ()
   "Start `magit-gh-pulls-mode' only after a manual request."
   (interactive)
