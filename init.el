86292b5ca47ce1c83493bec01ac5db5956d0f6fe
Andrew Cates <catesandrew@gmail.com>
2015-07-10 15:09:56 -0700

    Add smart beginning of line

--- init.el
+++ init.el
@@ -2231,6 +2231,28 @@ mouse-3: go to end"))))
 (bind-key "C-x p" 'pop-to-mark-command)
 (setq set-mark-command-repeat-pop t)
 
+;; The commands defined in this layer are taken from various sources like
+;; [[https://github.com/bbatsov/prelude][Prelude]].
+(defun dotemacs-smart-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+(global-set-key (kbd "C-a") 'dotemacs-smart-move-beginning-of-line)
+
 (setq scroll-margin 3                   ; 0 to drag the point along while scrolling
       scroll-conservatively 1000        ; Never recenter the screen while scrolling
       scroll-error-top-bottom t         ; Move to beg/end of buffer before
