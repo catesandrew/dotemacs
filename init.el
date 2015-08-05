9bdde8b2b8400a99bdeba6a64976e599e2d452b8
Andrew Cates <catesandrew@gmail.com>
2015-08-05 11:28:13 -0700

    Added Asciidoc contrib layer
    It has keybindings (very rudimentary, a lot more can be added later, see comments in source code). It has :defer t It has all its dependencies (cl mainly) properly defined All necessary init functions are prepared Moved to new `!lang` dir Battle-tested by daily usage

--- init.el
+++ init.el
@@ -9032,6 +9032,40 @@ If `end' is nil `begin-or-fun' will be treated as a fun."
   (progn
     (push 'company-nixos-options company-backends-nix-mode)))
 
+;; asciidoc
+(use-package adoc-mode
+  ;; We will NOT default `.txt' files to AsciiDoc mode,
+  ;; and `.asciidoc' extension is just plain stupid.
+  :mode (("\\.adoc?$" . adoc-mode))
+  :defer t
+  :ensure t
+  :config
+  (progn
+    ;; We have quite a lot of possible keybindings.
+    ;; See `adoc-mode.el', its bottom part where the huge easy-menu
+    ;; is defined and after that, where the various `tempo-template-*'
+    ;; functions are defined.
+
+    ;; See /doc/CONVENTIONS.md#plain-text-markup-languages
+    (evil-leader/set-key-for-mode 'adoc-mode
+      "mh1" 'tempo-template-adoc-title-1
+      ;; Alternative method of inserting top-level heading
+      "mhI" 'tempo-template-adoc-title-1
+      "mh2" 'tempo-template-adoc-title-2
+      ;; Alternative method of inserting the most usual heading
+      "mhi" 'tempo-template-adoc-title-2
+      "mh3" 'tempo-template-adoc-title-3
+      "mh4" 'tempo-template-adoc-title-4
+      "mh5" 'tempo-template-adoc-title-5
+      "mxb" 'tempo-template-adoc-strong
+      "mxi" 'tempo-template-adoc-emphasis)
+    ;; yes, exactly like that. To "promote" title is to INCREASE its size.
+    ;; `adoc-denote' does the opposite: increases its LEVEL,
+    ;; which DECREASES its size.
+    (define-key adoc-mode-map (kbd "M-h") 'adoc-denote)
+    ;; see the comment about  adoc-denote above
+    (define-key adoc-mode-map (kbd "M-l") 'adoc-promote)))
+
 
 ;;; Syntax Checking
 (use-package init-syntax-checking
