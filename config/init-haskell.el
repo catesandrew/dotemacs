111f4f8baa551880a03b3b73c62e43a218153e69
Andrew Cates <catesandrew@gmail.com>
2015-09-10 18:57:49 -0700

    disable line highlighting when shm is enabled

--- config/init-haskell.el
+++ config/init-haskell.el
@@ -1,7 +1,10 @@
 (defun dotemacs-init-haskell-mode ()
   ;; use only internal indentation system from haskell
   (if (fboundp 'electric-indent-local-mode)
-      (electric-indent-local-mode -1)))
+      (electric-indent-local-mode -1))
+        (when haskell-enable-shm-support
+          ;; in structured-haskell-mode line highlighting creates noise
+          (setq global-hl-line-mode nil)))
 
 (defun dotemacs-haskell-process-do-type-on-prev-line ()
   (interactive)
