a29d98a5508e817d56d20d1f6a4e8c974abd11cf
Andrew Cates <catesandrew@gmail.com>
2016-01-11 09:00:24 -0800

    fix last search buffer

--- config/init-helm.el
+++ config/init-helm.el
@@ -82,11 +82,14 @@ Removes the automatic guessing of the initial value based on thing at point. "
 (defun dotemacs-last-search-buffer ()
   "open last helm-ag or hgrep buffer."
   (interactive)
-  (if (get-buffer "*helm ag results*")
-      (switch-to-buffer-other-window "*helm ag results*")
-      (if (get-buffer "*hgrep*")
-          (switch-to-buffer-other-window "*hgrep*")
-          (message "No previous search buffer found"))))
+  (cond ((get-buffer "*helm ag results*")
+         (switch-to-buffer-other-window "*helm ag results*"))
+        ((get-buffer "*helm-ag*")
+         (helm-resume "*helm-ag*"))
+        ((get-buffer "*hgrep*")
+         (switch-to-buffer-other-window "*hgrep*"))
+        (t
+         (message "No previous search buffer found"))))
 
 (defvar dotemacs-helm-display-help-buffer-regexp '("*.*Helm.*Help.**"))
 (defvar dotemacs-helm-display-buffer-regexp `("*.*helm.**"
