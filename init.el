ccd44970c3ce03921c60c7727afc39d09581ab92
Andrew Cates <catesandrew@gmail.com>
2016-01-17 19:59:47 -0800

    git: new magit-blame micro state

--- init.el
+++ init.el
@@ -7725,13 +7725,26 @@ If called with a prefix argument, uses the other-window instead."
       (dotemacs-magit-set-repo-dirs-from-projectile))
 
     (evil-leader/set-key
-      "gb" 'magit-blame
-      "gB" 'magit-blame-quit
+      "gb" 'dotemacs/git-blame-micro-state
       "gl" 'magit-log-all
       "gL" 'magit-log-buffer-file
       "gs" 'magit-status
       "gd" 'dotemacs-magit-diff-head
-      "gC" 'magit-commit))
+      "gC" 'magit-commit)
+
+    (dotemacs-define-micro-state git-blame
+      :doc (concat "Press [b] again to blame further in the history, "
+                   "[q] to go up or quit.")
+      :on-enter (let (golden-ratio-mode) (call-interactively 'magit-blame))
+      :persistent t
+      :bindings
+      ("b" magit-blame)
+      ;; here we use the :exit keyword because we should exit the
+      ;; micro-state only if the magit-blame-quit effectively disable
+      ;; the magit-blame mode.
+      ("q" nil :exit (progn
+                       (magit-blame-quit)
+                       (not (bound-and-true-p magit-blame-mode))))))
   :config
   (progn
     ;; seems to be necessary at the time of release
