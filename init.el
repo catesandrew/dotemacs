105d4ae75509d632d9434f4ef5e7cf2ac813f0eb
Andrew Cates <catesandrew@gmail.com>
2016-01-18 23:09:14 -0800

    initialize desktop mode

--- init.el
+++ init.el
@@ -2129,23 +2129,82 @@ the user activate the completion manually."
 
 ;; http://stackoverflow.com/a/4485083/740527
 (use-package init-desktop
-  :load-path "config/"
-  :defer t
-  :bind ("C-c d d" . dotemacs-enable-desktop))
+  :load-path "config/")
 
 (use-package desktop                    ; Save buffers, windows and frames
+  :defer t
   :init
   (setq desktop-dirname (concat dotemacs-cache-directory "desktop/")
-        desktop-path                (list desktop-dirname)
-        ; desktop-save                t
-        desktop-files-not-to-save   "^$" ;reload tramp paths
-        desktop-load-locked-desktop nil)
+        desktop-base-file-name (concat "emacs_" emacs-version-short
+                                       ".desktop")
+        desktop-base-lock-name (concat "emacs_" emacs-version-short
+                                       ".desktop.lock")
+        desktop-path (list desktop-dirname)
+        desktop-save t
+        desktop-files-not-to-save "^$" ;reload tramp paths
+        desktop-load-locked-desktop nil
+        ;; Fix the frameset warning at startup
+        desktop-restore-frames nil
+        ;; Save desktops a minute after Emacs was idle.
+        desktop-auto-save-timeout 60)
   (desktop-save-mode 0)
-  :config (progn ;; Save desktops a minute after Emacs was idle.
-            (setq desktop-auto-save-timeout 60)
+  :config
+  (progn
+    ;; https://github.com/purcell/emacs.d/blob/master/lisp/init-sessions.el
+    ;; Save a bunch of variables to the desktop file.
+    ;; For lists, specify the length of the maximal saved data too.
+    (setq desktop-globals-to-save
+          (append '((comint-input-ring . 50)
+                    desktop-missing-file-warning
+                    (dired-regexp-history . 20)
+                    (extended-command-history . 30)
+                    (face-name-history . 20)
+                    (file-name-history . 100)
+                    (ido-buffer-history . 100)
+                    (ido-last-directory-list . 100)
+                    (ido-work-directory-list . 100)
+                    (ido-work-file-list . 100)
+                    (magit-read-rev-history . 50)
+                    (minibuffer-history . 50)
+                    (org-refile-history . 50)
+                    (org-tags-history . 50)
+                    (query-replace-history . 60)
+                    (read-expression-history . 60)
+                    (regexp-history . 60)
+                    (regexp-search-ring . 20)
+                    register-alist
+                    (search-ring . 20)
+                    (shell-command-history . 50)
+                    tags-file-name
+                    tags-table-list)))
 
-            (dolist (mode '(magit-mode git-commit-mode))
-              (add-to-list 'desktop-modes-not-to-save mode))))
+    ;; Don't save .gpg files. Restoring those files in emacsclients causes
+    ;; a problem as the password prompt appears before the frame is loaded.
+    (setq desktop-files-not-to-save
+          (concat "\\(^/[^/:]*:\\|(ftp)$\\)" ; original value
+                  "\\|\\(\\.gpg$\\)"
+                  "\\|\\(\\.plstore$\\)"
+                  "\\|\\(\\.desktop$\\)"
+                  "COMMIT_EDITMSG\\'"
+                  ;; If backup files with names like "file.sv.20150619_1641.bkp"
+                  ;; are saved to the desktop file, emacsclient crashes at launch
+                  ;; Need to debug why that's the case. But for now, simply not
+                  ;; saving the .bkp files to the desktop file is a workable
+                  ;; solution -- Fri Jun 19 16:45:50 EDT 2015 - kmodi
+                  "\\|\\(\\.bkp$\\)"
+                  "\\|\\(\\TAGS$\\)"))
+
+    ;; Don't save the eww buffers
+    (setq desktop-buffers-not-to-save (concat desktop-buffers-not-to-save
+                                              "\\|\\(^eww\\(<[0-9]+>\\)*$\\)"))
+
+    (dolist (mode '(magit-mode magit-log-mode))
+      (add-to-list 'desktop-modes-not-to-save mode))))
+
+(dotemacs-use-package-add-hook ignoramus
+  :post-config
+  (setq desktop-files-not-to-save (concat desktop-files-not-to-save
+                                         ignoramus-boring-file-regexp)))
 
 (use-package writeroom-mode             ; Distraction-free editing
   :ensure t
