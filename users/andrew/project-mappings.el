d78e4ff65cf773f3d2030d5ecfd964c067457fd7
Andrew Cates <catesandrew@gmail.com>
2016-01-23 01:16:20 -0800

    remove unused files

--- users/andrew/project-mappings.el
+++
@@ -1,8 +0,0 @@
-;; Emacs
-
-(defun custom-persp/emacs ()
-  (interactive)
-  (custom-persp "emacs"
-                (find-file "~/.emacs.d/init.el")))
-
-(define-key persp-mode-map (kbd "C-x p e") 'custom-persp/emacs)
