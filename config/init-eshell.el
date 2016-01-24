780e21d860ecd84d6c663c912f3d5bd2e7f687da
Andrew Cates <catesandrew@gmail.com>
2016-01-24 08:20:44 -0800

    fix eshell linum-mode check

--- config/init-eshell.el
+++ config/init-eshell.el
@@ -100,7 +100,7 @@ is achieved by adding the relevant text properties."
 (defun dotemacs-init-eshell ()
   "Stuff to do when enabling eshell."
   (setq pcomplete-cycle-completions nil)
-  (if linum-mode (linum-mode -1))
+  (if (bound-and-true-p linum-mode) (linum-mode -1))
   (unless dotemacs-shell-enable-smart-eshell
     ;; we don't want auto-jump to prompt when smart eshell is enabled.
     ;; Idea: maybe we could make auto-jump smarter and jump only if the
