2e4e9f14a16e1043c0311c197a280d0fe660b207
Andrew Cates <catesandrew@gmail.com>
2016-01-24 21:17:04 -0800

    comment out perf measurments

--- init.el
+++ init.el
@@ -676,25 +676,25 @@ group by projectile projects."
 (defvar dotemacs-really-kill-emacs nil
   "prevent window manager close from closing instance.")
 
-;; perf measurments
-(with-current-buffer (get-buffer-create "*Require Times*")
-  (insert "| feature | timestamp | elapsed |\n")
-  (insert "|---------+-----------+---------|\n"))
+;; ;; perf measurments
+;; (with-current-buffer (get-buffer-create "*Require Times*")
+;;   (insert "| feature | timestamp | elapsed |\n")
+;;   (insert "|---------+-----------+---------|\n"))
 
-(defadvice require (around require-advice activate)
-  (let ((elapsed)
-        (loaded (memq feature features))
-        (start (current-time)))
-    (prog1
-        ad-do-it
-      (unless loaded
-        (with-current-buffer (get-buffer-create "*Require Times*")
-          (goto-char (point-max))
-          (setq elapsed (float-time (time-subtract (current-time) start)))
-          (insert (format "| %s | %s | %f |\n"
-                          feature
-                          (format-time-string "%Y-%m-%d %H:%M:%S.%3N" (current-time))
-                          elapsed)))))))
+;; (defadvice require (around require-advice activate)
+;;   (let ((elapsed)
+;;         (loaded (memq feature features))
+;;         (start (current-time)))
+;;     (prog1
+;;         ad-do-it
+;;       (unless loaded
+;;         (with-current-buffer (get-buffer-create "*Require Times*")
+;;           (goto-char (point-max))
+;;           (setq elapsed (float-time (time-subtract (current-time) start)))
+;;           (insert (format "| %s | %s | %f |\n"
+;;                           feature
+;;                           (format-time-string "%Y-%m-%d %H:%M:%S.%3N" (current-time))
+;;                           elapsed)))))))
 
 
 ;;; Load Paths
