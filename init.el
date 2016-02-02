36275d1717da47adbd68ea90923c2d5ed418a47b
Andrew Cates <catesandrew@gmail.com>
2016-02-01 21:03:00 -0800

    scheme implementation selection keybinding

--- init.el
+++ init.el
@@ -5071,7 +5071,8 @@ It will toggle the overlay under point or create an overlay of one character."
       "sF" 'geiser-eval-definition-and-go
       "se" 'geiser-eval-last-sexp
       "sr" 'geiser-eval-region
-      "sR" 'geiser-eval-region-and-go)))
+      "sR" 'geiser-eval-region-and-go
+      "ss" 'geiser-set-scheme)))
 
 (when (eq dotemacs-completion-engine 'company)
   (dotemacs-use-package-add-hook company
