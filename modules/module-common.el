a4d7755be47f2ee32eeefc5af06c2bcdd0dae7e7
Andrew Cates <catesandrew@gmail.com>
2016-02-05 04:08:40 -0800

    chore: more load yasnippet to common module

--- modules/module-common.el
+++ modules/module-common.el
@@ -13,6 +13,26 @@
 ;;; List functions
 
 
-(provide 'module-common)
+(defun dotemacs-load-yasnippet ()
+  (unless yas-global-mode
+    (progn
+      (yas-global-mode 1)
+      (let ((private-yas-dir (if dotemacs-ac-private-snippets-directory
+                                 dotemacs-ac-private-snippets-directory
+                               (concat dotemacs-private-dir "/snippets/")))
+            (dotemacs-snippets-dir
+             (expand-file-name
+              (concat user-emacs-directory "snippets/"))))
+        (setq yas-snippet-dirs
+              (append (list private-yas-dir)
+                      (when (boundp 'yas-snippet-dirs)
+                        yas-snippet-dirs)
+                      dotemacs-snippets-dir))
+        (yas-load-directory dotemacs-snippets-dir t)
+        (yas-load-directory private-yas-dir t)
+        (setq yas-wrap-around-region t))))
+  (yas-minor-mode 1))
 
+
+(provide 'module-common)
 ;;; module-common.el ends here
