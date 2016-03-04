b196e1e6b2ed6376a3491fa9a29ec23ac5bb603c
Andrew Cates <catesandrew@gmail.com>
2016-03-04 13:06:40 -0800

    fix reference to ensime refactor
    rename: ensime-refactor-inline-local → ensime-refactor-diff-inline-local

--- modules/module-scala.el
+++ modules/module-scala.el
@@ -253,7 +253,7 @@ point to the position of the join."
         "ns"     'ensime
         "nS"     'ensime-gen-and-restart
 
-        "rd"     'ensime-refactor-inline-local
+        "rd"     'ensime-refactor-diff-inline-local
         "rD"     'ensime-undo-peek
         "rf"     'ensime-format-source
         "ri"     'ensime-refactor-organize-imports
