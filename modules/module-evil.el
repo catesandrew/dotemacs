a88ecf1cc12ebf0e3c0c2d56496289757972325f
Andrew Cates <catesandrew@gmail.com>
2016-03-04 14:17:01 -0800

    chore: rewrite evil smart-doc-lookup
    - and smart-goto-binding
    
    Prefer checking explicitly for the key binding rather that using
    condition-case and assuming all errors are related to a missing binding.

--- modules/module-evil.el
+++ modules/module-evil.el
@@ -273,9 +273,10 @@ Example: (evil-map visual \"<\" \"<gv\")"
         i.e. `SPC m h h`, to lookup the source of the definition,
         while falling back to `evil-lookup'"
       (interactive)
-      (condition-case-unless-debug nil
-          (execute-kbd-macro (kbd (concat dotemacs-leader-key " mhh")))
-        (error (evil-lookup))))
+      (let ((binding (key-binding (kbd (concat dotemacs-leader-key " mhh")))))
+        (if (commandp binding)
+            (call-interactively binding)
+          (evil-lookup))))
     (define-key evil-normal-state-map (kbd "K") 'dotemacs/evil-smart-doc-lookup)
 
     (defun dotemacs/evil-smart-goto-definition ()
@@ -284,9 +285,10 @@ Example: (evil-map visual \"<\" \"<gv\")"
         i.e. `SPC m g g`, to lookup the source of the definition,
         while falling back to `evil-goto-definition'"
       (interactive)
-      (condition-case-unless-debug nil
-          (execute-kbd-macro (kbd (concat dotemacs-leader-key " mgg")))
-        (error (evil-goto-definition))))
+      (let ((binding (key-binding (kbd (concat dotemacs-leader-key " mgg")))))
+        (if (commandp binding)
+            (call-interactively binding)
+          (evil-goto-definition))))
     (define-key evil-normal-state-map
       (kbd "gd") 'dotemacs/evil-smart-goto-definition)
 
