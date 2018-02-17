dcaaa5aa9426f4da2f14ac3b86773f339869f627
Andrew Cates <catesandrew@gmail.com>
2018-02-17 07:54:06 -0800

    rjsx mode indent close tags

--- layers/cats-javascript/funcs.el
+++ layers/cats-javascript/funcs.el
@@ -344,14 +344,6 @@
 
 ;; rjsx
 
-;; Inspired by http://blog.binchen.org/posts/indent-jsx-in-emacs.html
-(defun cats//js-jsx-indent-line-align-closing-bracket ()
-  "Workaround sgml-mode and align closing bracket with opening bracket"
-  (save-excursion
-    (beginning-of-line)
-    (when (looking-at-p "^ +\/?> *$")
-      (delete-char sgml-basic-offset))))
-
 (defun cats//rjsx-delete-creates-full-tag-with-insert (args)
   (interactive "p")
   (rjsx-delete-creates-full-tag args)
--- layers/cats-javascript/packages.el
+++ layers/cats-javascript/packages.el
@@ -45,18 +45,28 @@
     :defer t
     :init
     (progn
-      ;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+      (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
       (add-to-list 'auto-mode-alist '("\\.react.js\\'" . rjsx-mode))
       (add-to-list 'auto-mode-alist '("\\index.android.js\\'" . rjsx-mode))
       (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . rjsx-mode))
-      ;; (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . rjsx-mode))
-      ;; (add-to-list 'magic-mode-alist '("^import React" . rjsx-mode))
-
-      (advice-add #'js-jsx-indent-line
-                  :after
-                  #'cats//js-jsx-indent-line-align-closing-bracket))
+      (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . rjsx-mode))
+      (add-to-list 'magic-mode-alist '("^import React" . rjsx-mode)))
     :config
     (progn
+      ;; Inspired by http://blog.binchen.org/posts/indent-jsx-in-emacs.html
+      ;; Workaround sgml-mode and align closing bracket with opening bracket
+      (defadvice js-jsx-indent-line (after js-jsx-indent-line-after-hack activate)
+        "Workaround sgml-mode and follow airbnb component style."
+        (let* ((cur-line (buffer-substring-no-properties
+                          (line-beginning-position)
+                          (line-end-position))))
+          (if (string-match "^\\( +\\)\/?> *$" cur-line)
+              (let* ((empty-spaces (match-string 1 cur-line)))
+                (replace-regexp empty-spaces
+                                (make-string (- (length empty-spaces) sgml-basic-offset) 32)
+                                nil
+                                (line-beginning-position) (line-end-position))))))
+
       (evil-define-key 'insert rjsx-mode-map (kbd "C-d")
         'cats//rjsx-delete-creates-full-tag-with-insert)
 
