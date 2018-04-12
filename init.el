a66b53cbdbdba67963b8b750e19e2914f26a3403
Andrew Cates <catesandrew@gmail.com>
2018-04-12 07:09:05 -0700

    try hunspell for spell checking

--- init.el
+++ init.el
@@ -622,7 +622,26 @@ before packages are loaded. If you are unsure, you should try in setting them in
 
   (setq mark-ring-max 64)
   (setq kill-ring-max 200)
-  (setq global-mark-ring-max 128))
+  (setq global-mark-ring-max 128)
+
+  ;; find aspell and hunspell automatically
+  ;; http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs.html
+  (cond
+    ;; try hunspell at first, if hunspell does NOT exist, use aspell
+    ((executable-find "hunspell")
+      (setq ispell-program-name "hunspell")
+      (setq ispell-local-dictionary "en_US")
+      (setq ispell-local-dictionary-alist
+        ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters
+        ;; passed to hunspell You could use `("-d" "en_US,en_US-med")` to check
+        ;; with multiple dictionaries
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
+           )))
+    ((executable-find "aspell")
+      (setq ispell-program-name "aspell")
+      ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
+      (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
+  )
 
 (defun dotspacemacs/user-config ()
   "Configuration function for user code.
