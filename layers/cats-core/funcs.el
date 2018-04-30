7f71366fd103e62173dc48894becd229787a7c75
Andrew Cates <catesandrew@gmail.com>
2018-04-30 06:11:00 -0700

    nit remove unused function

--- layers/cats-core/funcs.el
+++ layers/cats-core/funcs.el
@@ -183,13 +183,6 @@ symbols, emojis, greek letters, as well as fall backs for."
   "Helper function to extract the name of a `FRAME'."
   (frame-parameter frame 'cats//projectile-curr))
 
-(defun cats//dabbrev-from-projectile (&optional dir)
-  "Use ."
-  (add-to-list 'directory-abbrev-alist
-               (cons
-                (concat "^" (directory-file-name (or dir (projectile-project-root))))
-                (file-name-nondirectory (directory-file-name (or dir (projectile-project-root)))))))
-
 (defun cats//do-nothing ()
   "A function that does nothing.")
 
