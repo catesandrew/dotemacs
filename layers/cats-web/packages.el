f2567917276152005b034d77962466ddd36f9e73
Andrew Cates <catesandrew@gmail.com>
2026-05-14 12:03:22 -0700

    chore: remove css sort

--- layers/cats-web/packages.el
+++ layers/cats-web/packages.el
@@ -8,11 +8,6 @@
   '(
     css-eldoc
     css-mode
-     (css-sort-buffer :location local)
-     (css-sort :location (recipe
-                           :fetcher github
-                           :repo "diiq/css-sort.el"))
-    com-css-sort
     emmet-mode
     flycheck
     helm-css-scss
@@ -22,28 +17,6 @@
     ;; xah-css-mode
     yasnippet
     ))
-
-
-;; com-css-sort
-(defun cats-web/init-com-css-sort ()
-  (use-package com-css-sort
-    :commands (com-css-sort-attributes-block com-css-sort-attributes-document)
-    :init
-    (progn
-      (setq com-css-sort-sort-type 'type-sort))
-    ))
-
-
-;; css-sort-buffer
-(defun cats-web/init-css-sort-buffer ()
-  (use-package css-sort-buffer
-    :commands (css-sort-buffer)))
-
-
-;; css-sort-buffer
-(defun cats-web/init-css-sort ()
-  (use-package css-sort
-    :commands (css-sort-attributes)))
 
 
 ;; org
